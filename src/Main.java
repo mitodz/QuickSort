766eff48efabfb520419dabe0934f5d88cc2029c
mitodz <mitodzzz@gmail.com>
2019-02-17 14:53:02 +0300

    Начало

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
+    public int partition(int[] a, int l, int r) {
+        int x = a[l];
+        int j = l;
+        for (int i = l + 1; i < r; i++) {
+            if (a[i]<=x) {
+                j++;
+                int buf = a[i];
+                a[i] = a[j];
+                a[j] = buf;
+            }
+        }
+        int buf = a[j];
+        a[j] = a[l];
+        a[l] = buf;
+        return j;
+    }
+
+    public void quickSort (int[] a, int l, int r) {
+        if (l > r) return;
+        int m = partition(a,l,r);
+        quickSort(a,l,m-1);
+        quickSort(a,m+1,r);
+    }
 
     public void run() {
         Scanner scanner = new Scanner("2 3\n" +
@@ -18,10 +42,15 @@ public class Main {
         for (int j = 0; j < m; j++) {
             p[j] = scanner.nextInt();
         }
-
+        System.out.println(Arrays.toString(a));
+        quickSort(a,1,a.length);
+        System.out.println(Arrays.toString(a));
     }
 
     public static void main(String[] args) {
-        // write your code here
+        long startTime = System.currentTimeMillis();
+        new Main().run();
+        long finishTime = System.currentTimeMillis();
+        System.out.println(finishTime - startTime + " ms");
     }
 }
